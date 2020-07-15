f9417d34430011aa44a54c4597758b1a631934f8
Strong duan <49770614+cupbaby@users.noreply.github.com>
2020-07-15 11:53:49 +0800

    增加前置环绕注释

--- src/com/pudong/advice/Advisor.java
+++ src/com/pudong/advice/Advisor.java
@@ -12,6 +12,7 @@ import org.springframework.aop.MethodBeforeAdvice;
 public class Advisor implements MethodBeforeAdvice{
 
 	@Override
+	/前置环绕
 	public void before(Method arg0, Object[] arg1, Object arg2) throws Throwable {
 		System.out.println(arg1.getClass().getName() + " want to try >>>>>>");
 	}
